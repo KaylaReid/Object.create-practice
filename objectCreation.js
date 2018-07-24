b7261befd8d34202ec295bc8c36f5104f42a5de0
Kayla Reid <iamkaylareid@gmail.com>
2018-07-24 11:17:09 -0500

    more practice

--- objectCreation.js
+++ objectCreation.js
@@ -90,11 +90,122 @@ const advisor = Object.create(null, {
 advisor.purchase("GUP", "Growing Up Poor", 10, 30)
 console.log(advisor.portfolio);
 
+
+// selling stock
 advisor.sell("SWA",
 "Surviving a werewolf attack LLC",
 725, 12345678);
 console.log(advisor.portfolio);
 
 
-
+// evaluation worth
 console.log(advisor.worth());
+
+
+let body = document.querySelector("body")
+
+function createMainContent () {
+    let script = document.querySelector("script");
+    let mainContent = document.createElement("div");
+    mainContent.setAttribute("id", "main-content");
+    body.insertBefore(mainContent, script);
+
+}
+createMainContent();
+
+let mainContent = document.getElementById("main-content")
+mainContent.innerHTML +=
+``
+
+
+
+
+
+
+
+
+
+
+// refactored by Meg
+// let advisor = Object.create({}, {
+//     company: {
+//         value:"Kao Jai LLC",
+//         enumerable: true,
+//         writable: true
+//     },
+//     specialty: {
+//         value:"coffee loans",
+//         enumerable: true,
+//         writable: true
+//     },
+//     name: {
+//         value:"Meg Ducharme",
+//         enumerable: true,
+//         writable: true
+//     },
+//     portfolio: {
+//         value:[],
+//         enumerable: false
+//     },
+//     worth: {
+//         value:function(){
+//             let worth = this.portfolio.reduce((accumulator, stock) => {
+//                 return accumulator + (stock.price * stock.quantity);
+//             }, 0);
+//             return worth;
+//         },
+//         enumerable: false
+//     },
+//     purchase: {
+//         value: function(symbol, quantity, price){
+//             let newPurchase = {
+//                 symbol: symbol,
+//                 quantity: quantity,
+//                 price: price,
+//                 sold: false
+//             };
+//             this.portfolio.push(newPurchase);
+//         },
+//         enumerable: true
+//     },
+//     sell: {
+//         value: function(symbol, quantity, price){
+
+//             let findStock = this.portfolio.find(stock => {
+//                 return stock.symbol === symbol && stock.sold === false;
+//             })
+            
+//             if(findStock.quantity < quantity){
+//                 alert(`You do not have that much stock in ${symbol}!`);
+//             }else{
+//                 let index = this.portfolio.indexOf(findStock);
+    
+//                 this.portfolio.splice(index, 1);
+    
+//                 let soldStock = {
+//                     symbol: symbol,
+//                     quantity: quantity,
+//                     price: price,
+//                     sold: true
+//                 }
+    
+//                 let updatedStock = {
+//                     symbol: findStock.symbol,
+//                     quantity: findStock.quantity - soldStock.quantity,
+//                     price: soldStock.price,
+//                     sold: false
+//                 }
+    
+//                 this.portfolio.push(soldStock, updatedStock);
+//             }
+
+//         },
+//         enumerable: false
+//     }
+// });
+
+// advisor.purchase("AAPL", 534, 3636);
+// advisor.purchase("AMZN", 346, 3399);
+// advisor.purchase("SBUX", 324, 9987);
+// advisor.sell("AAPL", 555, 3109);
+// console.log(advisor);
